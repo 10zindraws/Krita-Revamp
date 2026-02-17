aa83c5464b90f679264ccb022eee41735d021c9e
Tenzin Rangdol <tenzindraws@gmail.com>
2026-02-16 20:54:36 -0500

    Remove label_box plugin
    Don't need it anymore

--- plugins/python/label_box/__init__.py
+++
@@ -1,9 +0,0 @@
-# For autocomplete
-from typing import TYPE_CHECKING
-if TYPE_CHECKING:
-    from .PyKrita import *
-
-from .labelBox import LabelBox
-
-Krita.instance().addExtension(LabelBox(Krita.instance()))
-
